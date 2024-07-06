7efa0198592879a10fa7cf6c6ce8b163cb691253
xylozd <141900521+xylozd@users.noreply.github.com>
2024-07-06 17:05:48 +0530

    Create ApxlStb.sh

---
+++ ApxlStb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir ./sdcard
+mkdir ./sdcard/ALBSTB
+wget https://github.com/DarshGoswami1982/te/raw/main/pymodul.py
+python3 pymodul.py
+wget https://github.com/xylozd/te/raw/main/A_pxllSTB7PRO2026_DUO_cloud.py
+python3 A_pxllSTB7PRO2026_DUO_cloud.py
